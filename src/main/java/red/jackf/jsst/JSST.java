425b807485a4f08bd80b1ae8b6ec3d8e6cf0531a
Jack Pearce <jack@jackf.red>
2023-03-23 09:33:47 +0000

    Reset

--- src/main/java/red/jackf/jsst/JSST.java
+++ src/main/java/red/jackf/jsst/JSST.java
@@ -1,25 +1,14 @@
 package red.jackf.jsst;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import red.jackf.jsst.command.JSSTConfigCommand;
-import red.jackf.jsst.config.JSSTConfig;
-import red.jackf.jsst.features.portablecraftingtable.PortableCraftingTable;
-import red.jackf.jsst.features.portableshulkerbox.PortableShulkerBox;
-import red.jackf.jsst.features.wallediting.WallEditing;
 
 public class JSST implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("jsst");
-    public static final JSSTConfig.Handler CONFIG_HANDLER = new JSSTConfig.Handler();
 
     @Override
     public void onInitialize() {
-        CONFIG_HANDLER.load();
-        CommandRegistrationCallback.EVENT.register(JSSTConfigCommand::register);
-        PortableCraftingTable.setup();
-        WallEditing.setup();
-        PortableShulkerBox.setup();
+
     }
 }
--- src/main/java/red/jackf/jsst/features/portablecraftingtable/JSSTAlwaysValidable.java
+++
@@ -1,7 +0,0 @@
-package red.jackf.jsst.features.portablecraftingtable;
-
-import net.minecraft.world.InteractionHand;
-
-public interface JSSTAlwaysValidable {
-    void jsst_setAlwaysValid(InteractionHand usedHand);
-}
