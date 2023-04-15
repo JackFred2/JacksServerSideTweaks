ed0adff637e75e1f401de16cb6e733d33b2de550
Jack Pearce <jack@jackf.red>
2023-04-15 12:32:28 +0100

    Implemented the paginator to support modded enchantments/potions

--- src/main/java/red/jackf/jsst/features/itemeditor/menus/SelectorMenu.java
+++ src/main/java/red/jackf/jsst/features/itemeditor/menus/SelectorMenu.java
@@ -3,15 +3,19 @@ package red.jackf.jsst.features.itemeditor.menus;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import red.jackf.jsst.features.Sounds;
 import red.jackf.jsst.features.itemeditor.utils.CancellableCallback;
 import red.jackf.jsst.features.itemeditor.utils.EditorUtils;
 import red.jackf.jsst.features.itemeditor.utils.ItemGuiElement;
+import red.jackf.jsst.features.itemeditor.utils.Labels;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Presents a selection of options for a user to pick, along with a cancel option
+ * Presents a selection of options for a user to pick, along with a cancel option.
+ * It is highly recommended to use a {@link java.util.LinkedHashMap} or similar for order consistency.
  */
 public class SelectorMenu<T> {
     // a 9x5 page with 1 slot reserved for a cancel button
@@ -36,14 +40,33 @@ public class SelectorMenu<T> {
     protected void open() {
         var elements = new HashMap<Integer, ItemGuiElement>();
         elements.put(44, EditorUtils.cancel(callback::cancel));
+        var entries = options.entrySet();
         if (options.size() > SINGLE_PAGE_MAXIMUM) { // multi page
+            var toAdd = entries.stream().skip((long) page * OPTIONS_PER_PAGE).limit(OPTIONS_PER_PAGE).toList();
+            var index = 0;
+            for (var entry : toAdd) {
+                var slot = 9 * (index / 8) + index % 8;
+                elements.put(slot, new ItemGuiElement(entry.getValue(), () -> callback.accept(entry.getKey())));
+                index++;
+            }
 
+            if (page > 0)
+                elements.put(8, new ItemGuiElement(Labels.create(Items.RED_CONCRETE).withName("Previous Page").build(), () -> {
+                    page = Math.max(0, page - 1);
+                    Sounds.interact(player, 1f + ((float) (page + 1) / (maxPage + 1)) / 2);
+                    open();
+                }));
+            elements.put(17, new ItemGuiElement(Labels.create(Items.PAPER).withName("Page %s/%s".formatted(page + 1, maxPage + 1)).build(), () -> {}));
+            if (page < maxPage)
+                elements.put(26, new ItemGuiElement(Labels.create(Items.LIME_CONCRETE).withName("Next Page").build(), () -> {
+                    page = Math.min(maxPage, page + 1);
+                    Sounds.interact(player, 1f + ((float) (page + 1) / (maxPage + 1)) / 2);
+                    open();
+                }));
         } else { // single page
             var index = 0;
-            for (var e : options.entrySet()) {
-                elements.put(index++, new ItemGuiElement(e.getValue(), () -> {
-                    callback.accept(e.getKey());
-                }));
+            for (var entry : entries) {
+                elements.put(index++, new ItemGuiElement(entry.getValue(), () -> callback.accept(entry.getKey())));
             }
         }
 
