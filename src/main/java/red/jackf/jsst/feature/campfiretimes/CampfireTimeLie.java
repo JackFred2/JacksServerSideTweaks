33a316d545e7bf7197f08bce28ffcc69332f9266
Jack Pearce <jack@jackf.red>
2024-03-21 14:43:19 +0000

    [interal] campfire time refactor

--- src/main/java/red/jackf/jsst/feature/campfiretimes/CampfireTimeLie.java
+++ src/main/java/red/jackf/jsst/feature/campfiretimes/CampfireTimeLie.java
@@ -1,13 +1,31 @@
 package red.jackf.jsst.feature.campfiretimes;
 
+import com.google.common.collect.Lists;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Display;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3f;
 import red.jackf.jackfredlib.api.lying.Tracker;
 import red.jackf.jackfredlib.api.lying.entity.EntityLie;
+import red.jackf.jackfredlib.api.lying.entity.EntityUtils;
+import red.jackf.jackfredlib.api.lying.entity.builders.EntityBuilders;
 
 import java.util.List;
 
-public record CampfireTimeLie(Tracker<EntityLie<? extends Display>> tracker,
-                              List<EntityLie<Display.TextDisplay>> lies) {
+public final class CampfireTimeLie {
+    private static final float TEXT_SCALE = 0.6f;
+    private static final float TEXT_X_OFFSET = 0.3f;
+    private static final float TEXT_Y_OFFSET = 0.2f;
+    private final Tracker<EntityLie<? extends Display>> tracker;
+    private final List<EntityLie<Display.TextDisplay>> lies;
+
+    public CampfireTimeLie(Tracker<EntityLie<? extends Display>> tracker) {
+        this.tracker = tracker;
+        this.lies = Lists.newArrayList(null, null, null, null);
+    }
 
     public void fade() {
         for (EntityLie<Display.TextDisplay> lie : lies) {
@@ -23,11 +41,7 @@ public record CampfireTimeLie(Tracker<EntityLie<? extends Display>> tracker,
         return true;
     }
 
-    public void add(EntityLie<Display.TextDisplay> display, int slot) {
-        if (this.lies.get(slot) != null) {
-            this.lies.get(slot).fade();
-        }
-
+    private void add(EntityLie<Display.TextDisplay> display, int slot) {
         this.lies.set(slot, display);
         this.tracker.addLie(display);
         this.tracker.setRunning(false);
@@ -39,5 +53,26 @@ public record CampfireTimeLie(Tracker<EntityLie<? extends Display>> tracker,
             this.lies.get(slot).fade();
             this.lies.set(slot, null);
         }
+    }
+
+    public void setText(CampfireBlockEntity cbe, int slot, Component text) {
+        if (this.lies.get(slot) == null) {
+            int dir = (slot + cbe.getBlockState().getValue(CampfireBlock.FACING).get2DDataValue()) % 4;
+            Vec3 offset = new Vec3(
+                    dir == 1 || dir == 2 ? TEXT_X_OFFSET : -TEXT_X_OFFSET,
+                    TEXT_Y_OFFSET,
+                    dir == 2 || dir == 3 ? TEXT_X_OFFSET : -TEXT_X_OFFSET
+            );
+
+            this.add(EntityLie.builder(EntityBuilders.textDisplay((ServerLevel) cbe.getLevel())
+                    .billboard(Display.BillboardConstraints.CENTER)
+                    .scale(new Vector3f(TEXT_SCALE, TEXT_SCALE, TEXT_SCALE))
+                    .position(cbe.getBlockPos().getCenter().add(offset))
+                    .viewRangeModifier(1f / 16)
+                    .seeThrough(true)
+                    .build()).createAndShow(), slot);
+        }
+
+        EntityUtils.setDisplayText(this.lies.get(slot).entity(), text);
     }
 }
--- src/main/java/red/jackf/jsst/feature/campfiretimes/CampfireTimes.java
+++ src/main/java/red/jackf/jsst/feature/campfiretimes/CampfireTimes.java
@@ -1,20 +1,14 @@
 package red.jackf.jsst.feature.campfiretimes;
 
-import com.google.common.collect.Lists;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Display;
-import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
-import net.minecraft.world.phys.Vec3;
-import org.joml.Vector3f;
 import red.jackf.jackfredlib.api.lying.Tracker;
 import red.jackf.jackfredlib.api.lying.entity.EntityLie;
-import red.jackf.jackfredlib.api.lying.entity.EntityUtils;
-import red.jackf.jackfredlib.api.lying.entity.builders.EntityBuilders;
 import red.jackf.jsst.JSST;
 import red.jackf.jsst.feature.ToggleFeature;
 
@@ -25,9 +19,6 @@ public class CampfireTimes extends ToggleFeature<CampfireTimes.Config> {
     public static final CampfireTimes INSTANCE = new CampfireTimes();
     private CampfireTimes() {}
 
-    private static final float TEXT_SCALE = 0.6f;
-    private static final float TEXT_X_OFFSET = 0.3f;
-    private static final float TEXT_Y_OFFSET = 0.2f;
     private final Map<CampfireBlockEntity, CampfireTimeLie> LIES = new HashMap<>();
 
     @Override
@@ -65,32 +56,11 @@ public class CampfireTimes extends ToggleFeature<CampfireTimes.Config> {
                 lie = new CampfireTimeLie(Tracker.<EntityLie<? extends Display>>builder(serverLevel)
                         .setUpdateInterval(80L)
                         .setFocus(cbe.getBlockPos().getCenter(), 32.0)
-                        .build(false), Lists.newArrayList(null, null, null, null));
+                        .build(false));
                 LIES.put(cbe, lie);
             }
 
-            EntityLie<Display.TextDisplay> entityLie;
-            if (lie.lies().get(slot) == null) {
-                int dir = (slot + cbe.getBlockState().getValue(CampfireBlock.FACING).get2DDataValue()) % 4;
-                Vec3 offset = new Vec3(
-                        dir == 1 || dir == 2 ? TEXT_X_OFFSET : -TEXT_X_OFFSET,
-                        TEXT_Y_OFFSET,
-                        dir == 2 || dir == 3 ? TEXT_X_OFFSET : -TEXT_X_OFFSET
-                );
-                
-                entityLie = EntityLie.builder(EntityBuilders.textDisplay(serverLevel)
-                        .billboard(Display.BillboardConstraints.CENTER)
-                        .scale(new Vector3f(TEXT_SCALE, TEXT_SCALE, TEXT_SCALE))
-                        .position(cbe.getBlockPos().getCenter().add(offset))
-                        .viewRangeModifier(1f / 16)
-                        .seeThrough(true)
-                        .build()).createAndShow();
-                lie.add(entityLie, slot);
-            } else {
-                entityLie = lie.lies().get(slot);
-            }
-
-            EntityUtils.setDisplayText(entityLie.entity(), Component.literal(Mth.positiveCeilDiv(progress * 100, totalTime) + "%"));
+            lie.setText(cbe, slot, Component.literal(Mth.positiveCeilDiv(progress * 100, totalTime) + "%"));
         }
     }
 
