413b803b3fc361cc7aea049e877159d888dca6e0
Jack Pearce <jack@jackf.red>
2025-02-14 16:57:38 +0000

    [update] loom

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("fabric-loom") version "1.7-SNAPSHOT" apply false
+    id("fabric-loom") version "1.10-SNAPSHOT" apply false
 }
 stonecutter active "1.21.4" /* [SC] DO NOT EDIT */
 
